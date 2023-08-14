8d513c8596330f1060752030ccdcbe64d64213e5
Exterminator <87597629+masterexterminator@users.noreply.github.com>
2023-08-14 13:33:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,23 @@ let currentPage = 0;
 
 document.addEventListener('keydown', (event) => {
   if (event.key === 'ArrowUp') {
-    currentPage = Math.max(0, currentPage - 1);
+    changePage(Math.max(0, currentPage - 1));
   } else if (event.key === 'ArrowDown') {
-    currentPage = Math.min(pages.length - 1, currentPage + 1);
+    changePage(Math.min(pages.length - 1, currentPage + 1));
   }
-
-  pages.forEach((page, index) => {
-    if (index === currentPage) {
-      page.style.display = 'flex';
-    } else {
-      page.style.display = 'none';
-    }
-  });
 });
+
+function changePage(newPage) {
+  const currentPageElement = pages[currentPage];
+  const newPageElement = pages[newPage];
+
+  currentPageElement.style.opacity = 0; // Fade out the current page
+  setTimeout(() => {
+    currentPageElement.style.display = 'none';
+    newPageElement.style.display = 'flex';
+    setTimeout(() => {
+      newPageElement.style.opacity = 1; // Fade in the new page
+      currentPage = newPage;
+    }, 50); // Adjust the timing as needed
+  }, 300); // Adjust the timing as needed
+}
