a47343c2b4c4081e411545aac6782d360e8bd6ad
Exterminator <87597629+masterexterminator@users.noreply.github.com>
2023-08-15 10:05:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,19 @@
-let currentPageIndex = 0;
 const pages = document.querySelectorAll('.page');
+let currentPageIndex = 0;
+
+function showPage(index) {
+  pages[currentPageIndex].classList.remove('active');
+  pages[index].classList.add('active');
+  currentPageIndex = index;
+}
 
 document.addEventListener('keydown', (event) => {
-    if (event.key === 'ArrowUp') {
-        currentPageIndex = Math.max(currentPageIndex - 1, 0);
-        pages[currentPageIndex].classList.remove('slide-down');
-        pages[currentPageIndex].classList.add('slide-up');
-    } else if (event.key === 'ArrowDown') {
-        currentPageIndex = Math.min(currentPageIndex + 1, pages.length - 1);
-        pages[currentPageIndex].classList.remove('slide-up');
-        pages[currentPageIndex].classList.add('slide-down');
-    } else if (event.key === 'ArrowLeft' || event.key === 'ArrowRight') {
-        /* ... handle left/right navigation ... */
-    }
-
-    updatePageVisibility();
+  if (event.key === 'ArrowUp' && currentPageIndex > 0) {
+    showPage(currentPageIndex - 1);
+  } else if (event.key === 'ArrowDown' && currentPageIndex < pages.length - 1) {
+    showPage(currentPageIndex + 1);
+  }
 });
 
-    updatePageVisibility();
-});
-
-function updatePageVisibility() {
-    pages.forEach((page, index) => {
-        if (index === currentPageIndex) {
-            page.classList.add('visible');
-        } else {
-            page.classList.remove('visible');
-        }
-    });
-}
+// Show the initial page
+showPage(currentPageIndex);
